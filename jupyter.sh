e8b94eebf32e38371abfe2ce3caebd7557a3ade7
Jonathan Skowera <jskowera@gmail.com>
2022-01-27 23:20:55 -0800

    fix jupyter script so that jupyter will work with mujoco

--- jupyter.sh
+++ jupyter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+. ./venv/bin/activate
+
+export LD_LIBRARY_PATH="$HOME/.mujoco/mjpro150/bin:$HOME/.mujoco/mujoco210/bin:/usr/lib/nvidia"
+
 jupyter lab \
   --ip='127.0.0.1' \
   --port=8884 \
